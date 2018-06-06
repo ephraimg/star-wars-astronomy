b8870f892224672571fc60ed1c68c4ce17e740c6
ephraimg <ephraim.glick@gmail.com>
2018-06-06 11:09:16 +0100

    Test for Spinner during fetching

--- src/app/components/Search/Search.test.js
+++ src/app/components/Search/Search.test.js
@@ -5,10 +5,17 @@ import { expect } from 'chai';
 import { Search } from './Search';
 
 it('renders a button and input field', () => {
-
   const search = shallow(<Search />);
-
   expect(search.find('button')).to.have.length(1);
   expect(search.find('input')).to.have.length(1);
+});
 
+it('renders a Spinner if prop fetching === true', () => {
+  const search1 = shallow(<Search fetching={true}/>);
+  expect(search1.find('Spinner')).to.have.length(1);
+});
+
+it('doesn\'t render a Spinner if prop fetching !== true', () => {
+  const search1 = shallow(<Search fetching={false}/>);
+  expect(search1.find('Spinner')).to.have.length(0);
 });
